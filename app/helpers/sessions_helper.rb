79e491f678e680c47a9bcd53c2c379c05b1e3838
James Bodenschatz <enevolaprime@gmail.com>
2013-08-23 09:01:05 -0400

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,6 +16,7 @@ module SessionsHelper
     !current_user.nil?
   end
   
+  # study this - chapter 8
   def current_user=(user)
     @current_user = user
   end
@@ -23,5 +24,18 @@ module SessionsHelper
   def current_user
     remember_token = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
+  end
+  
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+  
+  def store_location
+    session[:return_to] = request.url
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,16 +15,15 @@ class User < ActiveRecord::Base
   before_create :create_remember_token
 
   validates :name,  presence: true, length: { maximum: 50 }
-  # validates(:name,  presence: true, length: { maximum: 50 })
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: {case_sensitive: false}
-  # validates(:email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false})
   
   has_secure_password #Rails method introduced in 3.1
-  validates :password, length: { minimum: 6 }
+  validates :password, length:   { minimum: 6 },
+                       presence: true
   
   def User.new_remember_token
     SecureRandom.urlsafe_base64
